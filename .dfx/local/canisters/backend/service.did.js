c57d3367ffd969d9ecd478baf375ec8061aa393f
Gabriel Ulloa <86376043+Gabriel-Ulloa@users.noreply.github.com>
2024-11-05 20:35:48 -0700

    Eliminar carpeta oculta del seguimiento

--- .dfx/local/canisters/backend/service.did.js
+++
@@ -1,7 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'getName' : IDL.Func([], [IDL.Text], ['query']),
-    'setName' : IDL.Func([IDL.Text], [], []),
-  });
-};
-export const init = ({ IDL }) => { return []; };
